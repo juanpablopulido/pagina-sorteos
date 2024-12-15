1e13cc75b0a2ae0e3ff26d84884b5f5f6a2ca1fe
juanpablo <juanpablopu10@gmail.com>
2024-12-15 15:18:38 -0500

    add serverless

--- functions/server.js
+++ functions/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Servir archivos estáticos desde la carpeta "public"
-app.use(express.static('public'));
-
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-// Ruta de la API
 app.post('/register', (req, res) => {
     const { name, address, phone } = req.body;
+
     if (!name || !address || !phone) {
         return res.status(400).json({ error: 'Todos los campos son obligatorios.' });
     }
+
     return res.status(200).json({ message: 'Registro exitoso.' });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+module.exports.handler = serverless(app);
